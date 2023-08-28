bce20b9bc37106b1bbc358b34f6e9ad3f0ac920b
Schyler Lowry <schyler_lowry@outlook.com>
2023-08-27 20:58:12 -0400

    Modify jQuery script to fix entry edit form.

--- fitness/forms.py
+++ fitness/forms.py
@@ -4,35 +4,42 @@ from datetime import datetime
 from bootstrap_datepicker_plus.widgets import DateTimePickerInput, TimePickerInput
 
 
-
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, Submit
 
 
 class WeightEntryForm(forms.ModelForm):
     """add weight entry form"""
-    
+
     class Meta:
         model = WeightEntry
         fields = ("weight", "recorded", "note")
         widgets = {
             'recorded': DateTimePickerInput(),
-            'weight': forms.TextInput(attrs={'maxlength': "5", "onkeypress": "return isNumberKey(event)", "required":""}),
+            'weight': forms.TextInput(attrs={'maxlength': "5", "onkeypress": "return isNumberKey(event)", "required": ""}),
             'note': forms.TextInput(attrs={'type': 'text'}),
         }
 
-
-        #options={"format": "HH:mm MM/DD/YYYY"}
-        #options={"format": "MM/DD/YYYY %I:%M %p"}
+        # options={"format": "HH:mm MM/DD/YYYY"}
+        # options={"format": "MM/DD/YYYY %I:%M %p"}
 
 
 class WeightEntryEditForm(forms.ModelForm):
     """edit weigh entry form"""
-    
-    
+
     class Meta:
         model = WeightEntry
         fields = ("weight", "recorded", "note")
         widgets = {
             'recorded': DateTimePickerInput(),
-            'weight': forms.TextInput(attrs={'maxlength': "5", "onkeypress": "return isNumberKey(event)", "required":""}),
+            'weight': forms.TextInput(attrs={'maxlength': "5", "onkeypress": "return isNumberKey(event)", "required": ""}),
             'note': forms.TextInput(attrs={'type': 'text'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.layout = Layout(
+            Fieldset('note', id="MYFORM_id",
+                     css_class="form-control", title="noteForm")
+        )
