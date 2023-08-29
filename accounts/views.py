db4ec8d35e42683e6acf31ecdbac08275df327c2
Schyler Lowry <schyler_lowry@outlook.com>
2023-08-29 11:57:10 -0400

    Match template styling on detail and list view. Update entry detail url to include 'detail' in the url. Add Javascript changes: -- make the collapse divs open independently -- ensure that the record weight modal form is empty whenever you click on it, regardless of what page you're on (hopefully)

--- accounts/views.py
+++ accounts/views.py
@@ -13,13 +13,15 @@ from fitness.forms import WeightEntryForm
 from fitness.views import WeightEntryPostView
 from fitness.models import WeightEntry
 
+
 class SignUpView(CreateView):
     """signup view"""
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
 
-class UserProfileView(LoginRequiredMixin, DetailView):
+
+class UserProfileView(DetailView):
     """user's profile view"""
     model = CustomUser
     template_name = "registration/user_profile.html"
@@ -35,14 +37,17 @@ class UserProfileView(LoginRequiredMixin, DetailView):
         context = super(UserProfileView, self).get_context_data(**kwargs)
 
         if self.request.user.is_authenticated:
-            postlist = WeightEntry.objects.all().filter(user=self.kwargs["pk"]).order_by("-recorded", "-updated","-created")
+            postlist = WeightEntry.objects.all().filter(
+                user=self.kwargs["pk"]).order_by("-recorded", "-updated", "-created")
         else:
-            postlist = WeightEntry.objects.all().order_by("-recorded", "-updated","-created")
+            postlist = WeightEntry.objects.all().order_by(
+                "-recorded", "-updated", "-created")
         context["postlist"] = postlist
         context["form"] = WeightEntryForm(initial={'recorded': datetime.now()})
         return context
-    
-class UserWeightEntryListView(LoginRequiredMixin, ListView):
+
+
+class UserWeightEntryListView(ListView):
     """user's profile view"""
     model = WeightEntry
     template_name = "registration/user_entry_list.html"
@@ -55,40 +60,45 @@ class UserWeightEntryListView(LoginRequiredMixin, ListView):
         return view(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
-        context = super(UserWeightEntryListView, self).get_context_data(**kwargs)
+        context = super(UserWeightEntryListView,
+                        self).get_context_data(**kwargs)
         customuser = CustomUser.objects.get(id=self.kwargs["pk"])
         if self.request.user.is_authenticated:
-            postlist = WeightEntry.objects.all().filter(user=self.kwargs["pk"]).order_by("-recorded", "-updated","-created")
+            postlist = WeightEntry.objects.all().filter(
+                user=self.kwargs["pk"]).order_by("-recorded", "-updated", "-created")
         else:
-            postlist = WeightEntry.objects.all().order_by("-recorded", "-updated","-created")
+            postlist = WeightEntry.objects.all().order_by(
+                "-recorded", "-updated", "-created")
         context["postlist"] = postlist
         context["customuser"] = customuser
         context["form"] = WeightEntryForm(initial={'recorded': datetime.now()})
         return context
-    
+
     def get_queryset(self):
         queryset = super(UserWeightEntryListView, self).get_queryset()
         if self.request.user.is_authenticated:
-            queryset = queryset.filter(user=self.kwargs["pk"]).order_by("-recorded", "-updated","-created")
+            queryset = queryset.filter(user=self.kwargs["pk"]).order_by(
+                "-recorded", "-updated", "-created")
         else:
-            queryset = WeightEntry.objects.all().order_by("-recorded", "-updated","-created")
+            queryset = WeightEntry.objects.all().order_by(
+                "-recorded", "-updated", "-created")
         return queryset
-    
-    
+
 
 class UserProfileChangeView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     """view for changing user profile info"""
     form_class = CustomUserChangeForm
     model = CustomUser
-    #success_url = reverse_lazy("user_profile")
+    # success_url = reverse_lazy("user_profile")
     template_name = "registration/user_change_profile.html"
-    
+
     def test_func(self):
         obj = self.get_object()
         return obj.pk == self.request.user.pk
-    
+
     def get_context_data(self, **kwargs):
         context = super(UserProfileChangeView, self).get_context_data(**kwargs)
-        context["form1"] = WeightEntryForm(initial={'recorded': datetime.now()})
-        
+        context["form1"] = WeightEntryForm(
+            initial={'recorded': datetime.now()})
+
         return context
--- fitness/urls.py
+++ fitness/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
-from.views import TemplateView, WeightEntryListView, WeightEntryUpdateView, WeightEntryDetailView, WeightEntryDeleteView, AllWeightEntryListView
+from .views import TemplateView, WeightEntryListView, WeightEntryUpdateView, WeightEntryDetailView, WeightEntryDeleteView, AllWeightEntryListView
 
 
 urlpatterns = [
-    path("/entries", AllWeightEntryListView.as_view(), name="weight_entry_list"),
-    path("<int:pk>/delete", WeightEntryDeleteView.as_view(), name="weight_entry_delete"),
-    path("<int:pk>/edit", WeightEntryUpdateView.as_view(), name="weight_entry_update"),
-    path("<int:pk>", WeightEntryDetailView.as_view(), name="weight_entry_detail"),
+    path("entries", AllWeightEntryListView.as_view(), name="weight_entry_list"),
+    path("<int:pk>/delete", WeightEntryDeleteView.as_view(),
+         name="weight_entry_delete"),
+    path("<int:pk>/edit", WeightEntryUpdateView.as_view(),
+         name="weight_entry_update"),
+    path("<int:pk>/detail", WeightEntryDetailView.as_view(),
+         name="weight_entry_detail"),
     path("", WeightEntryListView.as_view(), name="home"),
 ]
