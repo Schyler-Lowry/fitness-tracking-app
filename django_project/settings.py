d440219e69c658708a03d409e400c23deeba70a9
Schyler Lowry <schyler_lowry@outlook.com>
2023-12-31 11:11:01 -0500

    Settings adjustments.

--- django_project/settings.py
+++ django_project/settings.py
@@ -69,11 +69,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://10.0.0.155:5173",
-    "http://10.0.0.155:8000",
-]
+
 CORS_ALLOW_CREDENTIALS = True
 
 # CSRF_COOKIE_SECURE = False
